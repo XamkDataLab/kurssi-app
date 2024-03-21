c8ba5fc772fc9a1407d96583d8fa1a1f4622fc9e
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-03-22 01:04:00 +0200

    Update äppi.py

--- äppi.py
+++ äppi.py
@@ -6,44 +6,46 @@ from datanmuokkausfunktiot import *
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'>Datahaku julkaisuista</h1>", unsafe_allow_html=True)
 
-main_row = st.columns([2, 1, 2])
+# UI elements for input
+image_url = 'https://raw.githubusercontent.com/XamkDataLab/lens_demo/main/DALL.jpg'
+st.image(image_url)
 
-with main_row[0]:
-    image_url = 'https://raw.githubusercontent.com/XamkDataLab/lens_demo/main/DALL.jpg'
-    st.image(image_url)
-
-with main_row[1]:
-    start_date = st.date_input('Alkaen', value=pd.to_datetime('2024-01-01'))
-    end_date = st.date_input('Päättyen', value=pd.to_datetime('2024-03-01'))
-
-with main_row[2]:
-    terms = st.text_area('Hakutermit (erota pilkulla, operaattori OR)', 
-                         value='chatbot', 
-                         height=300).split(',')
-
-token = st.secrets["mytoken"]
+start_date = st.date_input('Alkaen', value=pd.to_datetime('2024-01-01'))
+end_date = st.date_input('Päättyen', value=pd.to_datetime('2024-03-01'))
+terms = st.text_area('Hakutermit (erota pilkulla, operaattori OR)', value='chatbot', height=300).split(',')
 
 if st.button('Hae Data'):
+    token = st.secrets["mytoken"]
     publication_data = get_publication_data(start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"), [term.strip() for term in terms], token)
+    
     if publication_data and publication_data['data']:
-        st.write(f"Löytyi {publication_data['total']} julkaisua")
-
         publications_df = publication_table(publication_data)
         fields_of_study_df = fields_of_study_table(publication_data)
 
+        st.write("DataFrames loaded successfully.")
         unique_fields_of_study = fields_of_study_df['field_of_study'].unique().tolist()
+        
         selected_field_of_study = st.selectbox('Select a Field of Study', ['All'] + unique_fields_of_study)
 
         if selected_field_of_study != 'All':
-            relevant_lens_ids = fields_of_study_df.loc[fields_of_study_df['field_of_study'] == selected_field_of_study, 'lens_id']
+        
+            relevant_lens_ids = fields_of_study_df[fields_of_study_df['field_of_study'] == selected_field_of_study]['lens_id'].tolist()
+            
+            
+            st.write(f"Relevant lens_ids for selected field: {relevant_lens_ids}")
+            
+            
             filtered_publications_df = publications_df[publications_df['lens_id'].isin(relevant_lens_ids)]
 
+            
             if not filtered_publications_df.empty:
                 st.dataframe(filtered_publications_df)
             else:
                 st.write("No publications found for the selected field of study.")
         else:
+            
             st.dataframe(publications_df)
     else:
         st.write("No publication data fetched. Please check your inputs and try again.")
+
 
